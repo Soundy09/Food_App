a05706c4cc4e5115bda96b72911d642c3a71577f
Lance Soundy <soundy09@gmail.com>
2023-03-14 13:58:37 -0400

    modified:   src/Components/Meals/MealItem/MealItem.js 	modified:   src/Components/Meals/MealItem/MealItemForm.js 	new file:   src/Components/Meals/MealItem/MealItemForm.module.css 	new file:   src/Components/UI/Input.js 	new file:   src/Components/UI/Input.module.css

---
+++ src/Components/UI/Input.js
@@ -0,0 +1,15 @@
+import classes from './Input.module.css';
+
+const Input = props => {
+    return 
+        <div className={classes.input}>
+            <label html={props.label.id} 
+            className={classes.label}>
+                {props.label}
+                    </label>
+            <input id={props.input.id} {...props.input}/>
+        </div>
+
+};
+
+export default Input;
