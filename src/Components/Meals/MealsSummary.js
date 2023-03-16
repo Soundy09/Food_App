4bcee1db931dd431abad05e41d954330e147f42d
Lance Soundy <soundy09@gmail.com>
2023-03-15 21:09:48 -0400

    modified:   src/Components/Meals/MealsSummary.js

--- src/Components/Meals/MealsSummary.js
+++ src/Components/Meals/MealsSummary.js
@@ -3,7 +3,7 @@ import classes from './MealsSummary.module.css';
 const MealsSummary = props => {
   return (
     <section className={classes.summary}>
-        <h2> Rosco's Famous Meals, Devivered Right To You!</h2>
+        <h2> Rosco's Famous Meals, A Fine Dining Experience</h2>
         <p>Choose anyone of Rosco's Fav meals, made from fresh and never frozen ingredients</p>
         <p>Rosco's prides himself on creating a wonderful dining experience that will leave you wanting seconds...</p>
     </section>
